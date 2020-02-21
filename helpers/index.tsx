740d28080ff7619a241f872fdb688fda5598d0aa
Mahesh Singh Chouhan <mahesh@futureshirts.com>
2020-02-21 20:15:06 +0530

    adding more clarify comments on helpers/draw.js and removing extra conditions which are being handled by reactJs

--- helpers/index.tsx
+++ helpers/index.tsx
@@ -9,29 +9,23 @@ export const checkProperties = (obj: any): boolean => {
 
 // Helper function to generate matrix of 0,1,2 on the basis of width, height and padding
 export const draw = (width: number, height: number, padding: number): number[][] => {
-    // Base case: if width and height less than or equal zero, returning empty array
-    if (width <= 0 || height <= 0) return [];
-
-    // if the height is less than 2 then render the last row
-    if (height < 2) return [Array(width).fill(1)];
-
-    // if the width and height less or equal padding + 2 
+    // Base case if width or height <= padding + 2 then drawing inner most box
     if (width <= padding + 2 || height <= padding + 2) {
         return [
-            Array(width).fill(1),
-            ...Array.from({ length: height - 2 }, () => width < 2 ? [2] : [2, ...Array(width - 2).fill(0), 2]),
-            Array(width).fill(1),
-        ];
+            [2, ...Array(width - 2).fill(1), 2], // Drawing first row
+            ...Array.from({length: height - 2}, () => [2, ...Array(width - 2).fill(0), 2]), // Drawing middle row's after height subtracting by 2 to handle borders
+            [2, ...Array(width - 2).fill(1), 2], // Drawing last row
+        ]
     }
 
-    // Returning array by calling draw function recursively by subtracting padding by 2 to handle the edges of inner arrays
+    // Returning array by calling draw fn recursively to create outer boxes
     return [
-        Array(width).fill(1),
-        ...Array.from({ length: padding / 2 }, () => [2, ...Array(width - 2).fill(0), 2]),
-        ...draw(width - padding - 2, height - padding - 2, padding).map((row) =>
-            [2, ...Array(padding / 2).fill(0), ...row, ...Array(padding >> 1).fill(0), 2]
+        [2, ...Array(width - 2).fill(1), 2], // Drawing first row
+        ...Array.from({length: padding / 2}, () => [2, ...Array(width - 2).fill(0), 2]), // Drawing top padding space row's after diving padding by 2
+        ...draw(width - padding - 2, height - padding - 2, padding).map(row => 
+            [2, ...Array(padding / 2).fill(0), ...row, ...Array(padding / 2).fill(0), 2] // Drawing inner boxes by calling draw fn recursively, also drawing left / right spaces by dividing padding by 2
         ),
-        ...Array.from({ length: padding / 2 }, () => [2, ...Array(width - 2).fill(0), 2]),
-        Array(width).fill(1)
-    ];
+        ...Array.from({length: padding / 2}, () => [2, ...Array(width - 2).fill(0), 2]), // Drawing bottom padding space row's after diving padding by 2
+        [2, ...Array(width - 2).fill(1), 2] // Drawing last row
+    ]
 }
